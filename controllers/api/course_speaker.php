84f5ced4bb72ae51f443ac9d653c94e66a9c8847
Phisitkarn Jinda <phisitkarn.jin@gmail.com>
2021-04-25 22:03:09 +0700

    Create course_speaker.php

---
+++ controllers/api/course_speaker.php
@@ -0,0 +1,34 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Content-Type: application/json');
+use OMCore\OMDb;
+$DB = OMDb::singleton();
+$log = new OMCore\OMLog;
+
+$response = array();
+$today = date("Y-m-d H:i:s");
+$exp = strtotime('+30 days', strtotime($today));
+$expires = date('Y-m-d H:i:s', $exp);
+
+$sql = "SELECT speaker_name, speaker_surname, speaker_position, speaker_company, speaker_image, speaker_stage
+        FROM v_course_speaker WHERE course_active = '1' order by speaker_stage";
+$sql_param = array();
+$ds = null;
+$res = $DB->query($ds, $sql, $sql_param, 0, -1, "ASSOC");
+$result = array();
+foreach($ds as $v) {
+        $result[] = array(
+                'speaker_name' => $v['speaker_name'],
+                'speaker_surname' => $v['speaker_surname'],
+                'speaker_position' => $v['speaker_position'],
+                'speaker_company' => $v['speaker_company'],
+                'speaker_image' => WEB_META_BASE_URL.'images/speaker/'.$v['speaker_image'],
+                'speaker_stage' => $v['speaker_stage']
+        );
+}
+$response['data'] = $result;
+//echo json_encode($response, JSON_UNESCAPED_SLASHES);
+
+echo json_encode($result);
+?>
