33da5d20aa503a6248f8247273d3a2c9b3888190
Phisitkarn Jinda <phisitkarn@cheesemobile.com>
2021-12-21 13:56:30 +0700

    ีีีuuu

--- controllers/api/course_speaker.php
+++ controllers/api/course_speaker.php
@@ -11,8 +11,8 @@ $today = date("Y-m-d H:i:s");
 $exp = strtotime('+30 days', strtotime($today));
 $expires = date('Y-m-d H:i:s', $exp);
 
-$sql = "SELECT speaker_name, speaker_surname, speaker_position, speaker_company, speaker_image, speaker_stage
-        FROM v_course_speaker WHERE course_active = '1' AND speaker_active = '1' order by speaker_stage";
+$sql = "SELECT speaker_name, speaker_surname, speaker_position, speaker_company, speaker_image, speaker_stage, speaker_order
+        FROM v_course_speaker WHERE course_active = '1' AND speaker_active = '1' order by speaker_stage, speaker_order";
 $sql_param = array();
 $ds = null;
 $res = $DB->query($ds, $sql, $sql_param, 0, -1, "ASSOC");
@@ -24,7 +24,8 @@ foreach($ds as $v) {
                 'speaker_position' => $v['speaker_position'],
                 'speaker_company' => $v['speaker_company'],
                 'speaker_image' => WEB_META_BASE_URL.'images/speaker/'.$v['speaker_image'],
-                'speaker_stage' => $v['speaker_stage']
+                'speaker_stage' => $v['speaker_stage'],
+                'speaker_order' => $v['speaker_order']
         );
 }
 $response['data'] = $result;
